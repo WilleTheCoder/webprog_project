ed54965fcb7715417bc98f4a48649fd3742534ae
daniel <daniel@znitz.se>
2023-03-01 14:22:46 +0100

    fungerande navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { MapComponent } from './map/map.component';
 import { errorComponent } from './components/error/error.component';
 
 const routes: Routes = [
-    {path: 'HomeComponent', component: HomeComponent},
-    {path: 'MapComponent', component: MapComponent},
+    {path: '', component: HomeComponent},
+    {path: 'Karta', component: MapComponent},
     { path: '**', component: errorComponent },  // Wildcard route for a 404 page
 ];
 
